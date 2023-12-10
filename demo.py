0046dd49703c2971b0128a0d83a7900aefdfa99c
Dewmidissanayake-2313079 <dewmidissanayake898@gmail.com>
2023-12-11 07:38:16 +0800

    Training the RandomForestClassifier for the Treatment dataset and split the data as training and testing data.

--- demo.py
+++ demo.py
@@ -1,1 +1,35 @@
-print("My first commit")
+# Import necessary libraries
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score, classification_report
+
+# Load your dataset
+df = pd.read_csv('Treatement dataset1.csv.')  # Replace 'your_dataset.csv' with the actual file path of your dataset
+
+# Convert categorical variables to one-hot encoding
+df_encoded = pd.get_dummies(df, columns=['Treatment Recommendation'], prefix='', prefix_sep='')
+
+# Assuming 'Level' is the target variable and the rest are features
+X = df_encoded.drop('Level', axis=1)
+y = df_encoded['Level']
+
+# Split the data into training and testing sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+# Initialize the Random Forest model with class_weight parameter
+rf_model = RandomForestClassifier(n_estimators=100, random_state=42, class_weight='balanced')
+
+# Train the model
+rf_model.fit(X_train, y_train)
+
+# Make predictions on the test set
+y_pred = rf_model.predict(X_test)
+
+# Evaluate the model
+accuracy = accuracy_score(y_test, y_pred)
+classification_report_output = classification_report(y_test, y_pred)
+
+# Print the results
+print(f"Accuracy: {accuracy:.2f}")
+print("Classification Report:\n", classification_report_output)
