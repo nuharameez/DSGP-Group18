8682bb0cfc4444cfec7f65ce132cd9fabe6d430a
nuharameez <127706172+nuharameez@users.noreply.github.com>
2024-04-03 22:10:26 +0530

    treatment according to age and gender

--- koa-app/backend/server.py
+++ koa-app/backend/server.py
@@ -1,10 +1,11 @@
 import os
 import numpy as np
-from flask import Flask, request, jsonify, send_from_directory
+from flask import Flask, request, jsonify, send_from_directory, send_file
 from tensorflow.keras.models import load_model
 from tensorflow.keras.preprocessing import image
 import cv2
-import pandas as pd
+import numpy as np
+import base64
 import joblib
 
 app = Flask(__name__, static_folder="build", static_url_path="/")
@@ -12,8 +13,10 @@ app = Flask(__name__, static_folder="build", static_url_path="/")
 UPLOAD_FOLDER = 'uploads'
 ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'}
 
+
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
 
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
@@ -29,14 +32,13 @@ knee_bone_model = load_model(knee_bone_model_path)
 severity_model_path = 'Custom_CNN_with_VGG16.h5'
 severity_model = load_model(severity_model_path)
 
+# Load the RandomForestClassifier model for treatment recommendation
+rf_model_path = 'random_forest_model_new.pkl'
+treatment_model = joblib.load(rf_model_path)
+
 # Define the categories
 categories = ['Doubtful: KL grading- 1', 'Minimal: KL grading- 2', 'Moderate: KL grading- 3', 'Extreme: KL grading- 4']
 
-# Load treatments dataframe
-df = pd.read_csv('treatments.csv')
-
-# Load the trained model
-severity_prediction_model = joblib.load('rf1.pkl')
 
 # Function to preprocess uploaded image
 def preprocess_image(image_path):
@@ -46,6 +48,7 @@ def preprocess_image(image_path):
     resized = cv2.resize(img_rgb, (img_size, img_size))
     return np.expand_dims(resized / 255.0, axis=0)
 
+
 # Function to get the grade for the image
 def get_grade(image_path):
     preprocessed_img = preprocess_image(image_path)
@@ -53,16 +56,40 @@ def get_grade(image_path):
     category_index = np.argmax(prediction)
     return categories[category_index]
 
+
 # Function to decode QR code and retrieve file path
 def decode_qr_code(qr_image):
     qr_detector = cv2.QRCodeDetector()
     data, _, _ = qr_detector.detectAndDecode(qr_image)
     return data.strip() if data else None
 
-# Function to get treatments for a given severity level
-def get_treatments(severity_level):
-    treatments = df[df['Severity'] == severity_level]['Treatment']
-    return treatments.tolist()
+
+def map_severity_to_grading(severity_level):
+    if severity_level.startswith('Doubtful'):
+        return 1
+    elif severity_level.startswith('Minimal'):
+        return 2
+    elif severity_level.startswith('Moderate'):
+        return 3
+    elif severity_level.startswith('Extreme'):
+        return 4
+    else:
+        return 0  # Default to Normal
+
+
+def get_treatments(severity_level, age_category, gender):
+    new_data = [[gender, age_category, severity_level]]  # Age, Gender, Severity
+    treatment = treatment_model.predict(new_data)
+    treatment_labels = {
+        0: 'Regular Exercises',
+        1: 'Proper lifting techniques',
+        2: 'Balanced Diet',
+        3: 'Physical Therapy exercises',
+        4: 'Aromatherapy'
+    }
+    treatment = treatment_labels[int(treatment[0])]
+    return treatment
+
 
 @app.after_request
 def after_request(response):
@@ -71,83 +98,88 @@ def after_request(response):
     response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
     return response
 
+
 @app.route('/analyze', methods=['POST'])
 def analyze_image():
     if 'file' not in request.files:
         return jsonify({'error': 'No file part'})
 
     file = request.files['file']
+    age_category = request.form.get('age_category', '')
+    gender_str = request.form.get('gender', '')
 
     if file.filename == '':
         return jsonify({'error': 'No selected file'})
 
+    if not (age_category and gender_str):
+        return jsonify({'error': 'Age category and gender are required'})
+
     if file and allowed_file(file.filename):
-        # Save the file
+        gender = 0 if gender_str.lower() == 'male' else 1
+
         filename = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
         file.save(filename)
 
-        # Decode QR code
         qr_image = cv2.imread(filename)
         qr_data = decode_qr_code(qr_image)
-
-        print("QR Code Data:", qr_data)  # Print QR code data
 
         if qr_data:
             image_path = qr_data
         else:
             image_path = filename
 
-        print("Image Path:", image_path)  # Print image path
-
-        # Load and preprocess the image for the knee model
         img_array = image.load_img(image_path, target_size=(224, 224))
         img_array = image.img_to_array(img_array)
-        img_array = np.expand_dims(img_array, axis=0) / 255.0  # Normalize pixel values
+        img_array = np.expand_dims(img_array, axis=0) / 255.0
 
-        # Perform knee bone identification
         knee_bone_prediction = knee_bone_model.predict(img_array)
-
-        # Map the knee bone prediction to 'Bone' or 'Not Bone'
         knee_bone_result = 'Not Bone' if knee_bone_prediction < 0.5 else 'Knee Bone Verified'
 
         if knee_bone_result == 'Not Bone':
-            # If not a knee bone, return the result
             result = {'knee_bone_result': 'Not a Knee Bone', 'image_path': image_path}
-            print(result)
             return jsonify(result)
 
-        # Perform knee model inference
         knee_prediction = knee_model.predict(img_array)
-
-        # Map the knee prediction to 'Normal' or 'Abnormal'
         knee_result = 'Normal' if knee_prediction < 0.5 else 'Abnormal'
 
         if knee_result == 'Normal':
-            # If the knee bone is normal, determine severity
-            severity = 0
-        else:
-            # If knee bone is abnormal, determine severity
-            severity_level = get_grade(image_path)
-            severity = int(severity_level.split("-")[-1].strip())
+            result = {'knee_bone_result': 'Knee Bone Verified', 'normal_result': 'Normal', 'image_path': image_path}
+            return jsonify(result)
 
-        # Get treatments for the determined severity level
-        treatments = get_treatments(severity)
+        severity = get_grade(image_path)
+        severity_level = map_severity_to_grading(severity)
 
-        result = {'knee_bone_result': 'Knee Bone Verified', 'normal_result': knee_result, 'severity': severity,
+        treatments = get_treatments(severity_level, age_category, gender)
+
+        result = {'knee_bone_result': 'Knee Bone Verified', 'normal_result': 'Abnormal', 'severity': severity,
                   'treatments': treatments, 'image_path': image_path}
-        print(result)
+
+        with open(image_path, "rb") as img_file:
+            img_bytes = img_file.read()
+            img_base64 = base64.b64encode(img_bytes).decode('utf-8')
+
+        result['image_base64'] = img_base64
+
         return jsonify(result)
 
     else:
         return jsonify({'error': 'File type not allowed'})
 
+
 @app.route('/uploads/<filename>')
 def uploaded_file(filename):
     return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
 
+
+@app.route('/get-image/<image_name>')
+def get_image(image_name):
+    return send_file(os.path.join(app.config['UPLOAD_FOLDER'], image_name))
+
+
 @app.route('/')
 def index():
     return app.send_static_file('index.html')
+
 
 if __name__ == '__main__':
     os.makedirs(app.config['UPLOAD_FOLDER'], exist_ok=True)
--- koa-app/src/App.js
+++ koa-app/src/App.js
@@ -1,70 +1,142 @@
-import React, { useState, useEffect } from 'react';
-import './App.css';
+import React, { useState } from 'react';
+import './App.css'; // Import the main CSS file
 import axios from 'axios';
 
+const LoginPage = ({ onLogin }) => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [isSignUp, setIsSignUp] = useState(false);
+  const [email, setEmail] = useState('');
+
+  const handleLogin = (event) => {
+    event.preventDefault();
+    // Perform login logic
+    onLogin();
+  };
+
+  const handleSignUp = (event) => {
+    event.preventDefault();
+    // Perform sign-up logic
+    console.log('Signed up with:', { username, email, password });
+  };
+
+  const toggleSignUp = () => {
+    setIsSignUp(!isSignUp);
+    console.log('isSignUp:', !isSignUp);
+  };
+
+  return (
+    <body-login>
+      <div className="sign-in-form" id="signInForm">
+        <h2>{isSignUp ? 'Sign Up' : 'Sign In'}</h2>
+        <form onSubmit={isSignUp ? handleSignUp : handleLogin}>
+          {isSignUp && (
+            <input
+              type="email"
+              id="email"
+              placeholder="Email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          )}
+          <input
+            type="text"
+            id="username"
+            placeholder="Username"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+          <input
+            type="password"
+            id="password"
+            placeholder="Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <button type="submit">{isSignUp ? 'Confirm' : 'Sign In'}</button>
+        </form>
+        <p>{isSignUp ? 'Already have an account?' : "Don't have an account?"}</p>
+        <button onClick={toggleSignUp}>{isSignUp ? 'Sign In' : 'Sign Up'}</button>
+      </div>
+    </body-login>
+  );
+};
+
 const App = () => {
+  const [showLogin, setShowLogin] = useState(true);
+  const [age, setAge] = useState(''); // Added state for age
+  const [gender, setGender] = useState(''); // Added state for gender
+
+  const handleLogin = () => {
+    setShowLogin(false);
+  };
+
   const [showModal, setShowModal] = useState(false);
   const [qrCode, setQrCode] = useState('');
   const [detectionResult, setDetectionResult] = useState('');
   const [normalResult, setNormalResult] = useState('');
   const [gradeResult, setGradeResult] = useState('');
-  const [allTreatments, setAllTreatments] = useState([]); // New state for all treatments
-  const [displayedTreatments, setDisplayedTreatments] = useState([]); // New state for displayed treatments
+  const [treatmentResult, setTreatmentResult] = useState('');
   const [imagePath, setImagePath] = useState('');
   const [fileToUpload, setFileToUpload] = useState(null);
   const [processing, setProcessing] = useState(false);
-
-  useEffect(() => {
-    if (allTreatments.length > 0) {
-      // Initially display 3 random treatments
-      const randomTreatments = getRandomTreatments(allTreatments, 3);
-      setDisplayedTreatments(randomTreatments);
-    }
-  }, [allTreatments]);
-
-  const getRandomTreatments = (treatments, num) => {
-    const shuffled = treatments.sort(() => 0.5 - Math.random());
-    return shuffled.slice(0, num);
-  };
+  const [imageUploaded, setImageUploaded] = useState(false);
 
   const handleModalClose = () => {
     setShowModal(false);
   };
 
   const handleFileUpload = async () => {
-    if (!fileToUpload) return;
+    if (!fileToUpload || !age || !gender) return; // Check if age and gender are provided
 
-    setQrCode(fileToUpload.name);
+    setDetectionResult('');
+    setNormalResult('');
+    setGradeResult('');
+    setTreatmentResult('');
+    setProcessing(true);
 
     const formData = new FormData();
     formData.append('file', fileToUpload);
+    formData.append('age_category', age); // Add age to form data
+    formData.append('gender', gender); // Add gender to form data
 
     try {
-      setProcessing(true);
-
-      const response = await axios.post('http://localhost:5000/analyze', formData);
+      const response = await axios.post('http://localhost:5000/analyze', formData, {
+        headers: {
+          'Content-Type': 'multipart/form-data'
+        }
+      });
       const analysisResult = response.data;
 
-      setDetectionResult(`${analysisResult.knee_bone_result}`);
-      setNormalResult(`Knee Bone is ${analysisResult.normal_result}`);
-      setGradeResult(`${analysisResult.severity}`);
+      setTimeout(() => {
+        setDetectionResult(`${analysisResult.knee_bone_result}`);
+      }, 700);
 
-      if (analysisResult.knee_bone_result === 'Not a Knee Bone') {
-        // If it's not a knee bone, clear treatments and return
-        setAllTreatments([]);
-        setDisplayedTreatments([]);
-        setImagePath(URL.createObjectURL(fileToUpload));
-        return;
+      setTimeout(() => {
+        setNormalResult(`Knee Bone is ${analysisResult.normal_result}`);
+      }, 1400);
+
+      setTimeout(() => {
+        setGradeResult(`${analysisResult.severity}`);
+      }, 2100);
+
+      let treatment = 'No treatment needed'; // Default treatment for normal knee
+      if (analysisResult.normal_result !== 'Normal') {
+        setTimeout(() => {
+          treatment = `Treatment: ${analysisResult.treatments}`;
+        }, 2800);
       }
 
-      setAllTreatments(analysisResult.treatments);
-      setDisplayedTreatments(getRandomTreatments(analysisResult.treatments, 5));
+      setTimeout(() => {
+        setTreatmentResult(treatment);
+      }, 2800);
 
-      setImagePath(URL.createObjectURL(fileToUpload));
+      setTimeout(() => {
+        setImagePath(`data:image/jpeg;base64,${analysisResult.image_base64}`);
+        setProcessing(false);
+      }, 3500);
     } catch (error) {
       console.error('Error analyzing image:', error);
-    } finally {
-      setProcessing(false);
     }
   };
 
@@ -72,62 +144,69 @@ const App = () => {
     const file = event.target.files[0];
     if (file) {
       setFileToUpload(file);
+      setImageUploaded(true);
+      setImagePath(URL.createObjectURL(file));
     }
   };
 
-  const handleRefresh = () => {
-    // Show another set of 5 random treatments
-    setDisplayedTreatments(getRandomTreatments(allTreatments, 3));
-  };
-
   return (
-    <div className="app-container">
-      <nav className="navigation">
-        <a href="#" className="logo">OsteoSense</a>
-        <ul className="nav-links">
-          <li><a href="#">Home</a></li>
-          <li><a href="#">About</a></li>
-          <li><a href="#">Contact</a></li>
-        </ul>
-      </nav>
-      <div className="content-container">
-        <div className="input-container">
-          <label htmlFor="imageInput" className="custom-icon" id="customIcon">
-            <img src="qricon.png" alt="Upload Image" />
-          </label>
-          <input type="file" id="imageInput" accept="image/*" style={{ display: 'none' }} onChange={handleInputChange} />
-          <div id="imageDisplay" onClick={() => document.getElementById('imageInput').click()}></div>
-          <button id="processButton" onClick={handleFileUpload}>Process Image</button>
-        </div>
-        <div className="result-container" id="resultContainer">
-          {processing && <p>Analyzing...</p>}
-          <div className="result-box">
-            <h3>Detection</h3>
-            <p id="detectionResult">{detectionResult}</p>
-          </div>
-          <div className="result-box">
-            <h3>Normal or Not</h3>
-            <p id="normalResult">{normalResult}</p>
-          </div>
-          <div className="result-box">
-            <h3>Severity</h3>
-            <p id="gradeResult">{gradeResult}</p>
-          </div>
-          {/* Display 3 random treatments */}
-          {displayedTreatments.length > 0 && (
-            <div className="result-box">
-              <h3>Treatment Recommendation</h3>
-              <ul>
-                {displayedTreatments.map((treatment, index) => (
-                  <li key={index}>{treatment}</li>
-                ))}
-              </ul>
-              <button onClick={handleRefresh}>Refresh</button> {/* Refresh button */}
+    <>
+      {showLogin ? (
+        <LoginPage onLogin={handleLogin} />
+      ) : (
+        <div className="app-container">
+          <nav className="navigation">
+            <a href="#" className="logo">OsteoSense</a>
+            <ul className="nav-links">
+              <li><a href="#">Home</a></li>
+              <li><a href="#">About</a></li>
+              <li><a href="#">Contact</a></li>
+            </ul>
+          </nav>
+          <div className="content-container">
+            <div className="input-container">
+              <input type="file" id="imageInput" accept="image/*" style={{ display: 'none' }} onChange={handleInputChange} />
+              <div id="imageDisplay" onClick={() => document.getElementById('imageInput').click()}>
+                {imageUploaded && <img src={imagePath} alt="Uploaded" style={{ maxWidth: '100%', maxHeight: '100%' }} />}
+              </div>
+              <label htmlFor="age" style={{ marginTop: '20px' }}>Age:</label>
+              <input type="text" id="age" value={age} onChange={(e) => setAge(e.target.value)} style={{ marginBottom: '10px', padding: '10px', borderRadius: '5px', border: '1px solid #ccc' }} />
+              <label htmlFor="gender" style={{ marginBottom: '10px' }}>Gender:</label>
+              <select id="gender" value={gender} onChange={(e) => setGender(e.target.value)} style={{ padding: '10px', borderRadius: '5px', border: '1px solid #ccc' }}>
+                <option value="">Select Gender</option>
+                <option value="male">Male</option>
+                <option value="female">Female</option>
+              </select>
+              <button id="processButton" onClick={handleFileUpload} disabled={!imageUploaded} style={{ marginTop: '10px', padding: '10px 20px', backgroundColor: '#15d39a', color: 'white', border: 'none', borderRadius: '5px', cursor: 'pointer', transition: 'background-color 0.3s ease' }}>Process Image</button>
             </div>
-          )}
+
+            <div className="result-container" id="resultContainer">
+              {processing && <p>Analyzing...</p>}
+              {!processing && (
+                <>
+                  <div className="result-box">
+                    <h3>Detection</h3>
+                    <p id="detectionResult">{detectionResult}</p>
+                  </div>
+                  <div className="result-box">
+                    <h3>Normal or Not</h3>
+                    <p id="normalResult">{normalResult}</p>
+                  </div>
+                  <div className="result-box">
+                    <h3>Severity</h3>
+                    <p id="gradeResult">{gradeResult}</p>
+                  </div>
+                  <div className="result-box">
+                    <h3>Treatment</h3>
+                    <p id="treatmentResult">{treatmentResult}</p>
+                  </div>
+                </>
+              )}
+            </div>
+          </div>
         </div>
-      </div>
-    </div>
+      )}
+    </>
   );
 };
 
